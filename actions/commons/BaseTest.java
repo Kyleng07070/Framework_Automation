1bc3484472124ef521e830676fb8d12c7ec21c9f
lap15050-local <lap15050-local@localhost>
2022-06-21 18:10:07 +0700

    Update BaseTest and XML file to run on multiple browsers

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -1,5 +1,28 @@
 package commons;
 
-public class BaseTest {
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BaseTest {
+	private WebDriver driverBaseTest;
+	private String projectPath = System.getProperty("user.dir");
+
+	protected WebDriver getBrowserDriver(String browserName) {
+		if (browserName.equals("firefox")) {
+			System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver-1");
+			driverBaseTest = new FirefoxDriver();
+		} else if (browserName.equals("chrome")) {
+			System.setProperty("webdriver.chrome.driver", projectPath + "/browserDrivers/chromedriver-5");
+			driverBaseTest = new ChromeDriver();
+		} else {
+			throw new RuntimeException("browserName invalid");
+		}
+
+		driverBaseTest.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driverBaseTest.get("https://demo.nopcommerce.com/");
+		return driverBaseTest;
+	}
 }
